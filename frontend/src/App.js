4edd353735c2ed36af8e776234bf38731304c1fa
guillermouzner <guillermouz16@gmail.com>
2022-10-12 13:14:30 -0300

    Auth page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
 import "./styles/App.scss";
+import { Route, Routes } from "react-router-dom";
 
 //COMPONENTS
 import PageContainer from "./components/Containers/PageContainer";
 import Navbar from "./components/Navbar/Navbar";
 import MobileNavbar from "./components/Navbar/MobileNavbar";
-import { Route, Routes } from "react-router-dom";
+
+//PAGES
+import Auth from "./pages/Auth";
 
 function App() {
     return (
         <div className="App">
-            <PageContainer>
+            <PageContainer optionClass={"pageContainer"}>
                 <Navbar />
                 <div className="mobileMenu">
                     {" "}
                     <MobileNavbar />
                 </div>
                 <Routes>
-                    <Route></Route>
+                    <Route>
+                        {/* AUTH PAGE */}
+                        <Route path="/auth" element={<Auth />} />
+                    </Route>
                 </Routes>
             </PageContainer>
         </div>
---
+++ frontend/src/pages/Auth.jsx
@@ -0,0 +1,70 @@
+import styles from "../styles/authComponents/Auth.module.scss";
+
+import { useEffect, useState } from "react";
+import MainContainer from "../components/Containers/MainContainer";
+import { Title } from "../components/Titles/Titles";
+
+const Auth = () => {
+    //LOGIN
+    const [email, setEmail] = useState("");
+    const [pw, setPw] = useState("");
+    //REGISTER
+    const [regEmail, setRegEmail] = useState("");
+    const [regPw, setRegPw] = useState("");
+    return (
+        <MainContainer>
+            {/* LOGIN */}
+            <form action="submit" onSubmit={(e) => e.preventDefault()}>
+                <div className={styles.container}>
+                    <Title>Login</Title>
+                    <span>Email:</span>
+                    <input
+                        type="email"
+                        autoComplete="username"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                    <span>Password:</span>
+                    <input
+                        type="password"
+                        onChange={(e) => setPw(e.target.value)}
+                        value={pw}
+                        autoComplete="password"
+                    />
+
+                    {/* LOGIN BTN */}
+                    <button>Login Now</button>
+                </div>
+            </form>
+            {/* REGISTER NOW */}
+            <form
+                action="submit"
+                onSubmit={(e) => e.preventDefault()}
+                className={styles.registerForm}
+            >
+                <div className={styles.container}>
+                    <Title>Register</Title>
+                    <span>Email:</span>
+                    <input
+                        type="email"
+                        autoComplete="email"
+                        onChange={(e) => setRegEmail(e.target.value)}
+                        value={regEmail}
+                    />
+                    <span>Password:</span>
+                    <input
+                        type="password"
+                        onChange={(e) => setRegPw(e.target.value)}
+                        value={regPw}
+                        autoComplete="new-password"
+                    />
+
+                    {/* REGISTER BTN */}
+                    <button>Register Now</button>
+                </div>
+            </form>
+        </MainContainer>
+    );
+};
+
+export default Auth;
