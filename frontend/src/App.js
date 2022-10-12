c60a3ace169ab470b9aac92bb14d6491d01776b9
guillermouzner <guillermouz16@gmail.com>
2022-10-12 01:07:12 -0300

    navbar-section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,23 @@ import "./styles/App.scss";
 
 //COMPONENTS
 import PageContainer from "./components/Containers/PageContainer";
+import Navbar from "./components/Navbar/Navbar";
+import MobileNavbar from "./components/Navbar/MobileNavbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <PageContainer>Hola</PageContainer>
+            <PageContainer>
+                <Navbar />
+                <div className="mobileMenu">
+                    {" "}
+                    <MobileNavbar />
+                </div>
+                <Routes>
+                    <Route></Route>
+                </Routes>
+            </PageContainer>
         </div>
     );
 }
