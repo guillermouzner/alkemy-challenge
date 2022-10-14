24b01917f527711dc8d79cf866a3b03060f6b9c2
guillermouzner <guillermouz16@gmail.com>
2022-10-13 22:23:52 -0300

    initial commit be

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import expressSession from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//Prisma Client
+import prisma from "./constats/config.js";
+
+const app = express();
+const port = process.env.SERVER_PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//SERVER CLIENT FOLDER IE REACT BUILD
+app.use(express.static(path.join(__dirname, "clientBuild")));
+
+//CORS
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "http://localhost:5000"],
+        methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE", "PATCH"],
+        credentials: true,
+    })
+);
+
+//SESSIONS
+app.use(
+    expressSession({
+        name: "sess",
+        secret: "secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new PrismaSessionStore(prisma, {
+            checkPeriod: 2 * 60 * 1000, //ms
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24,
+            httpOnly: true,
+            secure: false,
+        },
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`SERVER STARTED: http://localhost:${port}`);
+});
